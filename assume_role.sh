f877b9049f8aedf05758d74130cc3e482fed16b5
wildins <stephen.wilding@hotmail.co.uk>
2015-11-05 12:46:13 +0000

    Modifiled assime_role with more variables/options etc

--- assume_role.sh
+++ assume_role.sh
@@ -1,22 +1,63 @@
-function assume {
-  region=us-east-1
-  profile=$1
-  role=$2
-  token=$3
-  duration=900
-  if echo "$4" | grep "^[0-9]\{3,4\}$" > /dev/null; then duration=$4; fi
-  mfa_device="$(aws --profile $profile --region $region iam list-mfa-devices --query MFADevices[0].SerialNumber --output text  )"
-  result="$(aws --profile $profile --region $region \
-    sts assume-role --role-arn "$role" \
-    --serial-number "$mfa_device" --role-session-name "`whoami`" --duration-seconds "$duration" \
-    --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' --output text --token-code "$token" \
-  )"
-  if [ $? -ne 0 ]; then 
-      echo "Failed to assume role" 1>&2; return 1; 
-  else
-    export AWS_ACCESS_KEY_ID="`echo $result | awk '{ print $1 }'`"
-    export AWS_SECRET_ACCESS_KEY="`echo $result | awk '{ print $2 }'`"
-    export AWS_SESSION_TOKEN="`echo $result | awk '{ print $3 }'`"
-    return 0
-  fi
+usage () { 
+  echo "usage: assume -r role -t token -P profile -R region"
+}
+
+unassume () { 
+  unset token role profile region AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN;
+  echo "Role unassumed";
+  return 0
+}
+
+assume () 
+{ 
+    ROLEREF=${ROLEREF:-"$HOME/.aws/roles"};
+    local OPTIND P R r t opt;
+    while getopts ":r:t:R:P" opt; do
+        case $opt in 
+            P) profile=$OPTARG ;;
+            r) role=$OPTARG ;;
+            t) token=$OPTARG ;;
+            R) region=$OPTARG ;;
+            \?) echo "Invaid option: $OPTARG" 1>&2 && usage && return 1 ;;
+            :) echo "Option $OPTARG requires an arguement" 1>&2 && usage && return 1 ;;
+        esac;
+    done;
+    shift `expr $OPTIND - 1`;
+    #echo "DEBUG: roleref: $ROLEREF role: $role token: $token";
+    [ "$role" = "" ] && echo "You must supply a valid role" && usage && return 1;
+    if [ ! `echo $role | grep ^arn` ]; then
+        if [ ! -s $ROLEREF ]; then
+            echo "Invalid role selected: $role";
+            return 1;
+        fi;
+        role=`egrep "^$role[ 	]" $ROLEREF | awk '{print $2}'`;
+        [ "$role" = "" ] && echo "You must supply a valid role" && usage && return 1;
+    fi;
+    [ "$profile" = "" ] && profile=default;
+    [ "$region" = "" ] && region=eu-west-1;
+    if [ "$token" = "" ]; then
+        while true; do
+            echo -n "Please enter your MFA code> ";
+            read token junk;
+            if [ "$token" != "" ]; then
+                break;
+            fi;
+        done;
+    fi;
+    duration=900;
+    if echo "$4" | grep "^[0-9]\{3,4\}$" > /dev/null; then
+        duration=$4;
+    fi;
+    mfa_device="$(aws --profile $profile --region $region iam list-mfa-devices --query MFADevices[0].SerialNumber --output text  )";
+    result="$(aws --profile $profile --region $region     sts assume-role --role-arn "$role"     --serial-number "$mfa_device" --role-session-name "`whoami`" --duration-seconds "$duration"     --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' --output text --token-code "$token"   )";
+    if [ $? -ne 0 ]; then
+        echo "Failed to assume role" 1>&2;
+        return 1;
+    else
+        export AWS_ACCESS_KEY_ID="`echo $result | awk '{ print $1 }'`";
+        export AWS_SECRET_ACCESS_KEY="`echo $result | awk '{ print $2 }'`";
+        export AWS_SESSION_TOKEN="`echo $result | awk '{ print $3 }'`";
+        echo "Role $role assumed";
+        return 0;
+    fi
 }
